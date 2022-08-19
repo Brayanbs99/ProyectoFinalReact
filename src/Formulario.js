2f2778462eb53103a8c0d473ab5f0f2afab7c319
bryanbs2399@gmail.com <bryanbs2399@gmail.com>
2022-08-18 21:48:52 -0600

    Editar Cita

--- src/Formulario.js
+++ src/Formulario.js
@@ -5,6 +5,22 @@ export default function Formulario({pacientes,setPacientes}){
     const [fecha, setFecha]=useState("")
     const [sintomas, setSintomas]=useState("")
 
+    //Editar Cita
+
+    /*const [editarPaciente, setEditarPaciente] = useState("")
+    const initialFormState = { nombre: '', fecha: '', sintomas: '' }
+    const [currentCita, setCurrentCita] = useState(initialFormState)
+
+    const editRow = cita => {
+        setEditarPaciente(true) 
+        setCurrentCita({ nombre: cita.nombre, fecha: cita.fecha, sintomas: cita.sintomas })
+      }
+
+    const updateCita = (cita, updatedCita) => {
+        setEditarPaciente(false)
+        setCita(cita.map(cita => (cita.nombre === nombre ? updatedCita : cita)))
+    }*/
+
     function agregarPaciente(e){
         e.preventDefault()
         const paciente={
@@ -18,6 +34,25 @@ export default function Formulario({pacientes,setPacientes}){
         setFecha("")
         setSintomas("")
      }
+
+     /*const EditarPaciente = (props) => {
+
+        const paciente={
+            nombre,
+            fecha,
+            sintomas
+        }
+    
+        setValue('nombre', props.setCurrentCita.nombre)
+        setValue('fecha', props.setCurrentCita.fecha)
+        setValue('sintomas', props.setCurrentCita.sintomas)
+    
+        const onSubmit = (data, e) => {
+            data.nombre = props.setCurrentCita.nombre
+            console.log(data)
+            props.updateUser(props.setCurrentCita.nombre, data)
+            e.target.reset()
+        }*/
 
     return(
         <div className='mr-4'>
@@ -49,7 +84,19 @@ export default function Formulario({pacientes,setPacientes}){
                     className='bg-indigo-600 w-full p-3 text-white uppercase font-bold mt-2 '
                     type="submit"
                     value="Agregar Paciente" />
+
+            
+
+                EL ERROR QUE DA EL CODIGO ESTA EN LAS SIGUIENTES 4 LINEAS, POR FAVOR ELIMINARLAS YA QUE LAS DEJE
+            PARA QUE SE MUESTRE QUE INTENTE HACER EL PROCESO DE EDITAR LA CITA. LAS DISCULPAS DEL CASO,
+            NO ENCONTRÉ COMO COMENTAR ESTA PARTE.
+
+                <button type="submit">Editar paciente</button>
+                <button onClick={() => props.setEditarPaciente(false)} className="bg-indigo-600 w-full p-3 text-white uppercase font-bold mt-2">
+                    Editar
+                </button>
             </form>
         </div>
     )
+    //}
 }
